ae7f883433935c12348a3d0d6ac78a5de025ba9d
opsyp <57469962+opsyp@users.noreply.github.com>
2021-12-28 19:12:31 -0500

    Commented on the usage of useSate()

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 const App = () => {
+  //First argument is the state variable that is declared,
+  //second is the function that is called to update the state variable!
   const [usersList, setUsersList] = useState([]);
 
   const addUserHandler = (username, userage) => {
